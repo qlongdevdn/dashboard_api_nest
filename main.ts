3e16458a51b0638ba169ae4dbae9d5b458b4937b
qlongbanvien <ngumahobao@gmail.com>
2023-08-22 17:05:21 +0700

    Update first

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './src/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function main() {
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('Dashboard API')
+        .setDescription('DashboardAPI description')
+        .setVersion('1.0')
+        .addTag('dashboard')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('documents', app, document);
+
+    await app.listen(3000);
 }
-bootstrap();
+
+main();
